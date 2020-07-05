255af3ff960ef192f2fc5d35f01b6fe670cab493
Roman Pronskiy <roman@pronskiy.com>
2020-07-05 12:10:09 +0300

    Cleanup

--- injected.js
+++ injected.js
@@ -10,22 +10,7 @@ if (document.location.href.search("wiki.php.net/rfc") >= 0) {
         .replace('PHP RFC:', '[RFC]');
 
 } else
-if (document.location.href.search("feedly.com/i/subscription/feed") >= 0) {
-    linkObj = {
-        url: document.querySelector('div.entryHeader a').getAttribute("href"),
-        title: document.querySelector('div.entryHeader a.title').textContent,
-        description: ""
-    };
-
-    if (document.location.href.search("phpdeveloper.org") >= 0) {
-        linkObj.description = document.querySelector('div.entryBody div.content').innerHTML.trim();
-        var url = linkObj.description.substring(linkObj.description.lastIndexOf("Link: ") + 6);
-        if (url.indexOf('http') >= 0) {
-            linkObj.url = url;
-        }
-    }
-
-} else  if (document.location.href.search("phpdeveloper.org") >= 0) {
+if (document.location.href.search("phpdeveloper.org") >= 0) {
     linkObj = {
         url: document.querySelector('span.comment_count a').getAttribute("href"),
         title: document.querySelector('div.news_item a.title').textContent,
